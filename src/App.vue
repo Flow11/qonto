028322ed989dc645a84d5702bd93dc843376b92e
Florent Lagrede <florent.lagrede@gmail.com>
2017-04-22 15:32:37 +0200

    Coding interview
    The following PR contains:
    - Integration based on given mockup
    2 pages are available under `/` and `/transactions` path.
    
    - Fetching of data from transactions endpoint.
    
    Components created:
    - A SideMenu to navigate to the transactions page
    - A yellow header
    - A component that fetched the transactions and displays it into a table
      tag

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <SideMenu />
+    <MainHeader />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import SideMenu from './components/SideMenu'
+import MainHeader from './components/MainHeader'
+export default {
+  components: {
+    SideMenu,
+    MainHeader
+  },
+  name: 'app'
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+body {
+  margin: 0;
+}
+
+h1 {
+  margin: 0;
+}
+
+ul, li {
+  list-style: none;
+  margin: 0;
+	padding: 0;
+}
+
+a:visited {
+ color: inherit;
+}
+</style>
